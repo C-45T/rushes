cfac96f89c15d222582e1b316ce45746d00e048c
CastoR <castor3@gmail.com>
2017-11-21 11:47:54 +0100

    - add license and readme files - fix %YEAR% variable in src headers - add screenshot

--- src/gui/thumbnailview.h
+++ src/gui/thumbnailview.h
@@ -1,7 +1,7 @@
 /****************************************************************************
  *
  * Rushes! is a video cataloger application based on QtAv, OpenCV and FFMpeg.
- * Copyright (C) %YEAR% Remy Ruttner
+ * Copyright (C) 2017 Remy Ruttner
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
